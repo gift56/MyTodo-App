438537dbd1f67e945b2152de95454bf179156b1f
gift56 <efeasiughu@gmail.com>
2022-11-17 13:15:49 +0100

    imported task components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Task from "./components/Task";
 import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
     <div>
       <TaskForm />
+      <Task />
     </div>
   );
 }
