0f653f79500ae9e32c411ab57b2936c6051585f3
gift56 <efeasiughu@gmail.com>
2022-11-17 14:13:59 +0100

    created checked states

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsFillSquareFill, BsFillCheckSquareFill } from "react-icons/bs";
 
 const Checkbox = () => {
+  const [checked, setChecked] = useState(false);
+  
   return (
     <div>
       <div className="checkbox unchecked">
