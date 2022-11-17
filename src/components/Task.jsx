6f927a755f71f8ce871c107fbb88936978dbe28c
gift56 <efeasiughu@gmail.com>
2022-11-17 15:47:37 +0100

    updated some stuff

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,7 @@ import Checkbox from "./Checkbox";
 
 const Task = ({ name, done, onToggle }) => {
   return (
-    <div className="task">
+    <div className={`task ${done ? "done" : ""}`}>
       <Checkbox checked={done} onClick={() => onToggle(!done)} />
       {name}
     </div>
