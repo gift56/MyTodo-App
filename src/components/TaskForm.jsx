2f9f00f014242a9369f5a5a5aab5f00822c19b0f
gift56 <efeasiughu@gmail.com>
2022-11-17 13:09:44 +0100

    created state

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TaskForm = () => {
+  const [value, setValue] = useState("");
+  
   return (
     <form>
       <button>+</button>
