50fb31f682f3f31f621367cf388d600741401f30
gift56 <efeasiughu@gmail.com>
2022-11-17 12:56:50 +0100

    working on some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-
+      <p>Hello world</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
