3e96a2e9ea46a3e40f82a716e2a3bcd22fef46a8
gift56 <efeasiughu@gmail.com>
2022-11-17 12:59:48 +0100

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <form>
+        <button>+</button>
+        <input type="text" />
+      </form>
     </div>
   );
 }
